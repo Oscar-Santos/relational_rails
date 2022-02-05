671ff3afc9afd5bdb46c20ffde17fa06c025dfe0
Oscar Santos <oscar303data@gmail.com>
2022-02-05 12:51:15 -0700

    Refactor entrees index

--- spec/features/entrees/index_spec.rb
+++ spec/features/entrees/index_spec.rb
@@ -6,12 +6,17 @@ require 'rails_helper'
 # When I visit '/entrees_name'
 # Then I see each entree in the system including the Entree's attributes:
 
-RSpec.describe 'entre index' do
+RSpec.describe 'entree index' do
   it 'displays each entree and its attributes' do
-    entree_1 = Entree.create!(name: 'Papa rellena', hot_dish: true, calories: 367)
-    entree_2 = Entree.create!(name: 'Tiradito', hot_dish: false, calories: 287)
-    entree_3 = Entree.create!(name: 'Ocopa', hot_dish: false, calories: 199)
+    gaston = Chef.create!(name: 'Gaston Acurio', age: 40, is_male: true, years_employed: 11)
+    virgilio = Chef.create!(name: 'Virgilio Morales', age: 33, is_male: true, years_employed: 9)
 
-    visit '/entrees_name'
+    entree_1 = gaston.entrees.create!(name: 'Papa rellena', hot_dish: true, calories: 367)
+    entree_2 = virgilio.entrees.create!(name: 'Ocopa', hot_dish: false, calories: 199)
+
+    visit '/entrees'
+
+    expect(page).to have_content(entree_1.name)
+    expect(page).to have_content(entree_2.name)
   end
 end
