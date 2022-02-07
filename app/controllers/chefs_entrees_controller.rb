cfce506682da71ced5bc7fef79e9f1f6585e842a
Oscar Santos <oscar303data@gmail.com>
2022-02-06 19:38:33 -0700

    Fix chefs entrees controller, test pass

--- app/controllers/chefs_entrees_controller.rb
+++ app/controllers/chefs_entrees_controller.rb
@@ -1,6 +1,22 @@
+# class ChefsEntreesController < ApplicationController
+#   def index
+#     Chef.find(params[:id])
+#     chef_entrees = @chef.entrees
+#   end
+# end
+
+
+
+
+
+
+
+
+
 class ChefsEntreesController < ApplicationController
   def index
-    Chef.find(params["chef_id"])
+    chef = Chef.find(params["chef_id"])
+
     @chef_entrees = chef.entrees
   end
 end
