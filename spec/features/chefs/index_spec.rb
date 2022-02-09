e611de87acd92b22431df85b83065143ed2dd454
Oscar Santos <oscar303data@gmail.com>
2022-02-09 08:56:28 -0700

    Refactor US 6

--- spec/features/chefs/index_spec.rb
+++ spec/features/chefs/index_spec.rb
@@ -27,13 +27,13 @@ RSpec.describe 'chef index' do
 # I see that records are ordered by most recently created first
 # And next to each of the records I see when it was created
 
-  it 'wip show chef in order of created' do
+  it 'it shows chef in order of created' do
     chef_1 = Chef.create!(name: 'Jose Beltran', age: 45, is_male: true, years_employed: 8, created_at: 10.seconds.ago)
     chef_2 = Chef.create!(name: 'Gaston Acurio', age: 39, is_male: true, years_employed: 11, created_at: 15.seconds.ago)
     chef_3 = Chef.create!(name: 'Lena Rojas', age: 29, is_male: false, years_employed: 8, created_at: 20.seconds.ago)
 
     visit "/chefs"
-    
+
     expect(chef_1.name).to appear_before(chef_2.name)
     expect(chef_2.name).to appear_before(chef_3.name)
   end
